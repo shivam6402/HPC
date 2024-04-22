b07a5da0502315d649f10adeb9d464e6358f8bb0
Shivam Kudal <102711973+shivam6402@users.noreply.github.com>
2024-04-22 23:06:57 +0530

    trybubble.cpp

--- HPC Omp/trybubble.cpp
+++ HPC Omp/trybubble.cpp
@@ -1,49 +1,67 @@
 #include <iostream>
 #include <vector>
-#include <omp.h>
+#include <cstdlib>  // For rand()
+#include <ctime>    // For clock()
+#include <omp.h>    // For OpenMP
 
-using namespace std;
-
-void bubble_sort_odd_even(vector<int>& arr) {
-    bool isSorted = false;
-    while (!isSorted) {
-        isSorted = true;
-#pragma omp parallel for
-        for (int i = 0; i < arr.size() - 1; i += 2) {
-            if (arr[i] > arr[i + 1]) {
-                swap(arr[i], arr[i + 1]);
-                isSorted = false;
+// Sequential Bubble Sort
+void bubbleSortSequential(int* arr, int size) {
+    for (int i = 0; i < size - 1; i++) {
+        for (int j = 0; j < size - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
             }
         }
-#pragma omp parallel for
-        for (int i = 1; i < arr.size() - 1; i += 2) {
-            if (arr[i] > arr[i + 1]) {
-                swap(arr[i], arr[i + 1]);
-                isSorted = false;
+    }
+}
+
+// Parallel Bubble Sort using OpenMP
+void bubbleSortParallel(int* arr, int size) {
+    #pragma omp parallel for shared(arr, size)
+    for (int i = 0; i < size - 1; i++) {
+        for (int j = 0; j < size - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
             }
         }
     }
 }
 
 int main() {
-    int n;
-    cout << "Enter the number of elements: ";
-    cin >> n;
+    int size;
+    std::cout << "Enter the size of the array: ";
+    std::cin >> size;
 
-    vector<int> arr(n);
-    cout << "Enter " << n << " elements: ";
-    for (int i = 0; i < n; ++i) {
-        cin >> arr[i];
+    int arr[size];
+    std::cout << "Enter " << size << " integers:" << std::endl;
+    for (int i = 0; i < size; i++) {
+        std::cin >> arr[i];
     }
 
-    double start, end;
-    // Measure performance of parallel bubble sort using odd-even transposition
-    start = omp_get_wtime();
-    bubble_sort_odd_even(arr);
-    end = omp_get_wtime();
+    // Measure sequential bubble sort time
+    clock_t startSeqBubble = clock();
+    bubbleSortSequential(arr, size);
+    clock_t endSeqBubble = clock();
+    double timeSeqBubble = double(endSeqBubble - startSeqBubble) / CLOCKS_PER_SEC;
 
-    cout << "Parallel bubble sort using odd-even transposition time: " << end - start << endl;
+    // Measure parallel bubble sort time
+    int par_arr[size];
+    std::copy(arr, arr + size, par_arr);
+
+    clock_t startParBubble = clock();
+    bubbleSortParallel(par_arr, size);
+    clock_t endParBubble = clock();
+    double timeParBubble = double(endParBubble - startParBubble) / CLOCKS_PER_SEC;
+
+    // Print execution times
+    std::cout << "Sequential Bubble Sort Time: " << timeSeqBubble << " seconds" << std::endl;
+    std::cout << "Parallel Bubble Sort Time: " << timeParBubble << " seconds" << std::endl;
 
     return 0;
 }
+
 
